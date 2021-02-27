989eb1b693171f625a78a089f9f9f7e89b889f06
Nekko <nekkostreet@gmail.com>
2021-02-27 14:16:53 -0800

    Finished To Do List

--- app.js
+++ app.js
@@ -1,38 +1,52 @@
+const date = require(__dirname + '/date.js'); //is how we require other local js files as a module
+
 const bodyParser = require("body-parser");
+
 const express = require('express');
+
 const ejs = require('ejs');
+
 const { static } = require("express");
+
 const app = express();
-var items = ["Buy Food", "Cook Food"];
+
+const items = [];
+const workItems = [];
 
 app.set('view engine', 'ejs'); // Must be placed after app is declared,  reads our markers inside veiws doc
+
+
 app.use(bodyParser.urlencoded({ extended: true })) //here is how we use express to read another document
-    // this echos out what we see on our servers/site
+
+
 app.use(express.static("public"))
+
+
 app.get('/', function(req, res) {
-    var today = new Date(); // Some vanilla JS
-    var options = { weekday: 'long', day: 'numeric', month: 'long' };
+    let day = date.getDate();
+    // let day = date.getDay() this would return just the day of the week after we create our own module
+    res.render("list", { listTitle: day, newListItems: items })
 
-    var day = today.toLocaleDateString("en-US", options);
-
-
-    res.render("list", { kindOfDay: day, newListItems: items })
 });
-// this controls what happens when our button is pressed
-app.post("/", function(req, res) {
-    var item = req.body.newItems;
-    items.push(item); //array.action(what it is)
-    res.redirect("/");
-})
 
+// this controls what happens when our button is pressed on our to list
+app.post("/", function(req, res) {
+
+    let item = req.body.newItems;
+
+    if (req.body.list === "Work List") {
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+})
+app.get('/work', function(req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
 
 // this is the creation of our server
 app.listen(3000, function() {
     console.log("The server is being ran on port 3000");
 });
-// if (today.getDay() === 6 || today.getDay() === 0) { never forget that programmers count from zero
-//     day = "Weekend"
-// } else {
-//     day = "Weekday";
-//     res.sendFile(__dirname + "/weekday.html")
-// }
